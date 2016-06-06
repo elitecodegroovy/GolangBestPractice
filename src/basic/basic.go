774f55ad6772b8bbdff160a7a5b72ba490dc2d42
JOHN <elite_jigang@163.com>
2016-06-06 22:21:57 +0800

    Go concurrent

--- src/basic/basic.go
+++ src/basic/basic.go
@@ -5,9 +5,12 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
 	"sort"
+	"sync"
 	"syscall"
 	"text/tabwriter"
 	"time"
@@ -338,37 +341,143 @@ mustCopy(conn, os.Stdin)
 conn.Close()
 <- done  // wait for background goroutine to finish
 */
-func DoChannel() {
 
+func count(out chan<- int) {
+	for i := 0; i < 101; i++ {
+		out <- i
+	}
+	close(out)
+}
+
+func square(input <-chan int, output chan<- int) {
+	for n := range input {
+		output <- n * n
+	}
+	close(output)
+}
+
+func print(input <-chan int) {
+	for p := range input {
+		fmt.Print("\t", p)
+	}
+}
+
+/**
+// Counter
+go func() {
+	for x := 0; x < 101 ; x++ {
+		naturals <- x
+	}
+	close(naturals)
+}()
+// Squarer
+go func() {
+	for {
+		x, ok := <-naturals
+		if !ok {
+			break //channel was closed and drained
+		}
+		squares <- x * x
+	}
+	close(squares)
+}()
+// Printer (in main goroutine)
+for x := range squares {
+	fmt.Print("\t", x)
+}
+fmt.Println()
+*/
+func DoChannel() {
 	naturals := make(chan int)
 	squares := make(chan int)
-	// Counter
+	go count(naturals)
+	go square(naturals, squares)
+	print(squares)
+	fmt.Println()
+	DoConcurrent()
+}
+
+//Buffered Channels
+var verbose = flag.Bool("v", true, "show verbose progress messages")
+
+func DoConcurrent() {
+	// ...start background goroutine...
+
+	//!-
+	// Determine the initial directories.
+	flag.Parse()
+	roots := flag.Args()
+	if len(roots) == 0 {
+		roots = []string{"."}
+	}
+
+	// Traverse the file tree.
+	fileSizes := make(chan int64)
+	var n sync.WaitGroup
+	for _, root := range roots {
+		n.Add(1)
+		go walkDir(root, &n, fileSizes)
+	}
 	go func() {
-		for x := 0; ; x++ {
-			naturals <- x
-			if x == 100 {
-				break // stop the loop
-			}
-		}
-		close(naturals)
+		n.Wait()
+		close(fileSizes)
 	}()
-	// Squarer
-	go func() {
-		for {
-			x, ok := <-naturals
-			if !ok {
-				break //channel was closed and drained
-			}
-			squares <- x * x
-		}
-		close(squares)
-	}()
-	// Printer (in main goroutine)
+
+	//!+
+	// Print the results periodically.
+	var tick <-chan time.Time
+	if *verbose {
+		tick = time.Tick(1 * time.Millisecond)
+	}
+	var nfiles, nbytes int64
+loop:
 	for {
-		y := <-squares
-		fmt.Print("\t", y)
-		if y == 10000 {
-			break
+		select {
+		case size, ok := <-fileSizes:
+			if !ok {
+				break loop // fileSizes was closed
+			}
+			nfiles++
+			nbytes += size
+		case <-tick:
+			printDiskUsage(nfiles, nbytes)
 		}
 	}
+	printDiskUsage(nfiles, nbytes) // final totals
+}
+
+func printDiskUsage(nfiles, nbytes int64) {
+	fmt.Printf("%d files  %.1f GB\n", nfiles, float64(nbytes)/1e9)
+}
+
+// walkDir recursively walks the file tree rooted at dir
+// and sends the size of each found file on fileSizes.
+func walkDir(dir string, n *sync.WaitGroup, fileSizes chan<- int64) {
+	defer n.Done()
+	for _, entry := range dirents(dir) {
+		if entry.IsDir() {
+			n.Add(1)
+			subdir := filepath.Join(dir, entry.Name())
+			go walkDir(subdir, n, fileSizes)
+		} else {
+			fileSizes <- entry.Size()
+		}
+	}
+}
+
+// sema is a counting semaphore for limiting concurrency in dirents.
+var sema = make(chan struct{}, 20)
+
+// dirents returns the entries of directory dir.
+func dirents(dir string) []os.FileInfo {
+	sema <- struct{}{}        // acquire token
+	defer func() { <-sema }() // release token
+	// ...
+	//!-sema
+	entries, err := ioutil.ReadDir(dir)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "du: %v\n", err)
+		return nil
+	}
+	return entries
 }
